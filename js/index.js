d1de76bc2182ea7b5ef8336638217bebe31eabc2
TheEngineerOne <verbruggenlucas2@gmail.com>
2024-04-06 16:30:13 +0200

    adding new view for planets

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const apiUrl = "https://helldivers-2-dotnet.fly.dev"
 const planetsUrl = "/api/v1/planets"
 const planetsApi = apiUrl + planetsUrl
+var planet_view;
 
 async function request(url){
   let response = await fetch(url)
@@ -9,17 +10,33 @@ async function request(url){
     let json = await response.json()
     return(json)
   }else{
-    throw new Error('HTTP-ERROR : ' + response.status)
+    throw new Error('HTTP-ERROR : ' + response.status, {cause: response.status})
   }
 }
 
+async function planetViewLoader(){
+  let response = await fetch("view/planets.html")
+  let parser = new DOMParser()
+  planet_view = parser.parseFromString(response.text(),'text/html')
+}
+
+async function load(data){
+}
+
 window.onload = async () => {
+  let main_content = document.querySelector(".main_content")
+  planetViewLoader()
   try{
   let data = await request(planetsApi)
+  for(const element of data){
+    load(element)
+  }
   }catch(error){
     console.error(error)
-    fetch('view/error.html')
-      .then(response => response.text())
-      .then(text => document.body.innerHTML = text)
+    let error_display = await fetch('view/error.html')
+    main_content.innerHTML = await error_display.text()
+    if(error.cause != undefined){
+      document.querySelector("#ErrorMessage").innerText = error.cause
+    }
   }
 }
